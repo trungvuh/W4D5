647a830f922d681791de10fa9c181fefda5daae0
TV <trungvuh@gmail.com>
2017-10-31 12:01:24 -0700

    All

---
+++ W4D5/Music/spec/models/user_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+
+  describe 'validation' do
+
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:password_digest) }
+    it { should validate_length_of(:password).is_at_least(6) }
+  end
+
+  
+end
--- w5d2/Reddit/config/application.rb
+++ w5d2/Reddit/config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Music
+module Reddit
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
@@ -14,14 +14,5 @@ module Music
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.generators do |g|
-      g.test_framework :rspec,
-      :fixtures => false,
-      :view_specs => false,
-      :helper_specs => false,
-      :routing_specs => false,
-      :controller_specs => true,
-      :request_specs => false
-    end
   end
 end
