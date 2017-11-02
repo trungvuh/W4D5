88b309655e5971c1b39dcea3fc3799d389e765ef
TV <trungvuh@gmail.com>
2017-11-01 17:44:32 -0700

    Finish phase 3

--- w5d2/Reddit/app/models/sub.rb
+++ w5d2/Reddit/app/models/sub.rb
@@ -1,5 +1,21 @@
+# == Schema Information
+#
+# Table name: subs
+#
+#  id          :integer          not null, primary key
+#  title       :string           not null
+#  description :text             not null
+#  user_id     :integer          not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Sub < ApplicationRecord
-  validates :title, :description, :user, presence: true
+  validates :title, :description, :moderator, presence: true
+
+  has_many :post_subs, inverse_of: :sub, dependent: :destroy
+  has_many :posts, through: :post_subs, source: :post, inverse_of: :subs
+  # has_many :posts, inverse_of: :sub
 
   belongs_to :moderator,
     class_name: :User,
--- w5d2/Reddit/db/schema.rb
+++ w5d2/Reddit/db/schema.rb
@@ -10,10 +10,39 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171031183554) do
+ActiveRecord::Schema.define(version: 20171101001953) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
+
+  create_table "post_subs", force: :cascade do |t|
+    t.integer "sub_id", null: false
+    t.integer "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_post_subs_on_post_id"
+    t.index ["sub_id"], name: "index_post_subs_on_sub_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title", null: false
+    t.string "url"
+    t.text "content"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "subs", force: :cascade do |t|
     t.string "title", null: false
